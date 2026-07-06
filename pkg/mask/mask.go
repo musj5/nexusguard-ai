999a43084102372c249c24825ea845bd2eda42b0
Smile Spoon <80548715+smspoon@users.noreply.github.com>
2026-07-07 00:10:47 +0300

    Update mask.go

--- pkg/mask/mask.go
+++ pkg/mask/mask.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Mustafa Al-Aqrawi (Smile Spoon). All rights reserved.
+// Copyright 2026 Mustafa Al-Aqrawi (Smile Spoon). All rights reserved.
 // Use of this source code is governed by the MIT License
 // that can be found in the LICENSE file.
 
